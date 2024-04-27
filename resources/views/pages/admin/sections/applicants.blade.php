9af170e06df9d6ee6f1843220075fb7cc41c23e4
jvlguanco <jvlguanco@gmail.com>
2024-04-27 08:50:31 +0800

    Adjusted button for search

--- resources/views/pages/admin/sections/applicants.blade.php
+++ resources/views/pages/admin/sections/applicants.blade.php
@@ -65,15 +65,17 @@
 
             <form action="{{ url()->current() }}" method="GET" class="relative flex items-center mt-4 md:mt-0">
                 <span class="absolute">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mx-3 text-gray-400 ">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                    </svg>
+                    <button type="submit" class="mt-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mx-3 text-gray-400 ">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                        </svg>
+                    </button>
                 </span>
 
                 <input type="text" placeholder="Search" name="searchApplicant" class="block w-full py-1.5 pr-5 text-gray-700 bg-white border border-gray-200 rounded-lg md:w-80 placeholder-gray-400/70 pl-11 rtl:pr-11 rtl:pl-5 focus:border-blue-400  focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40">
                 <input type="hidden" name="type" value="{{ $type }}">
                 <input type="hidden" name="statusType" value="{{ $statusType }}">
-                <button type="submit" class="hidden">Search</button>
+                
             </form>
         </div>
 
