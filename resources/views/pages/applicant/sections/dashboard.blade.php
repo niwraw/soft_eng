93e268e4609c06df1fcee18c71f74538effe6e93
jvlguanco <jvlguanco@gmail.com>
2024-05-02 17:40:02 +0800

    started to fix applicant dash

---
+++ resources/views/pages/applicant/sections/dashboard.blade.php
@@ -0,0 +1,72 @@
+<div class="w-auto pt-8 pr-8">
+    <div class="grid w-auto bg-red-500" style="grid-template-columns: 3fr 1fr;">
+            <div class="flex flex-col items-center gap-8 pt-4">
+                <div class="grid w-11/12 bg-gray-100 border-2 border-gray-700 h-fit rounded-2xl" style="grid-template-columns: 1fr 1fr;">
+                    <div class="p-6">
+                        <h1 class="mb-4 text-lg font-semibold text-yellow-500">Personal Information</h1>
+
+                        <div class="w-full h-fit">
+                            <div class="grid mb-4" style="grid-template-columns: 1fr 2fr;">
+                                <h3>Surname:</h3>
+                                <p>Guanco</p>
+                            </div>
+                            <div class="grid mb-4" style="grid-template-columns: 1fr 2fr;">
+                                <h3>First Name:</h3>
+                                <p>John Vincent</p>
+                            </div>
+                            <div class="grid mb-4" style="grid-template-columns: 1fr 2fr;">
+                                <h3>Middle Name:</h3>
+                                <p>Lo</p>
+                            </div>
+                            <div class="grid mb-4" style="grid-template-columns: 1fr 2fr;">
+                                <h3>Extension Name:</h3>
+                                <p></p>
+                            </div>
+                            <div class="grid mb-4" style="grid-template-columns: 1fr 2fr;">
+                                <h3>Email:</h3>
+                                <p>jvlguanco@gmail.com</p>
+                            </div>
+                            <div class="grid mb-4" style="grid-template-columns: 1fr 2fr;">
+                                <h3>Phone Number:</h3>
+                                <p>09661550858</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="p-6">
+                        <h1 class="mb-4 text-lg font-semibold text-yellow-500">School Information</h1>
+
+                        <div class="w-full h-fit">
+                            <div class="grid mb-4" style="grid-template-columns: 1fr 2fr;">
+                                <h3>School:</h3>
+                                <p>Regional Science High School for Region VI</p>
+                            </div>
+                            <div class="grid mb-4" style="grid-template-columns: 1fr 2fr;">
+                                <h3>LRN:</h3>
+                                <p>1234-567890</p>
+                            </div>
+                            <div class="grid mb-4" style="grid-template-columns: 1fr 2fr;">
+                                <h3>School Email:</h3>
+                                <p>test@gmail.com</p>
+                            </div>
+                            <div class="grid mb-4" style="grid-template-columns: 1fr 2fr;">
+                                <h3>Program Choice 1:</h3>
+                                <p>BS Computer Science</p>
+                            </div>
+                            <div class="grid mb-4" style="grid-template-columns: 1fr 2fr;">
+                                <h3>Program Choice 2:</h3>
+                                <p>BS information Technology</p>
+                            </div>
+                            <div class="grid mb-4" style="grid-template-columns: 1fr 2fr;">
+                                <h3>Program Choice 3:</h3>
+                                <p>BS Statistics</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                
+            </div>
+        </div>
+    </div>
+</div>
