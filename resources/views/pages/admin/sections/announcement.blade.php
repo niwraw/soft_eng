96a4b0524f70cf9906dc5e9e340fcbad4c1399b5
jvlguanco <jvlguanco@gmail.com>
2024-05-12 15:17:25 +0800

    initialized table for announcements

--- resources/views/pages/admin/sections/announcement.blade.php
+++ resources/views/pages/admin/sections/announcement.blade.php
@@ -42,6 +42,67 @@
 
         <div class="pl-2">
             <h2 class="mb-4 text-lg font-medium text-indigo-700">Add/Edit Advisories</h2>
+
+            <div class="flex flex-col mt-6">
+                <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                        <div class="overflow-hidden border border-gray-200 md:rounded-lg">
+                            <table class="min-w-full divide-y divide-gray-200">
+                                <thead class="bg-gray-50 ">
+                                    <tr>
+                                        <th scope="col" class="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 ">
+                                            ID.
+                                        </th>
+
+                                        <th scope="col" class="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 ">
+                                            Date
+                                        </th>
+
+                                        <th scope="col" class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 ">
+                                            Announcement
+                                        </th>
+                                        
+                                        <th scope="col" class="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 ">
+                                            Actions
+                                        </th>
+                                    </tr>
+                                </thead>
+
+                                <tbody class="bg-white divide-y divide-gray-200 ">
+                                    <tr>
+                                        <td class="px-4 py-4 text-sm font-medium whitespace-nowrap">
+                                            asddas
+                                        </td>
+                                        
+                                        <td class="px-12 py-4 text-sm font-medium whitespace-nowrap">
+                                            asdasd
+                                        </td>
+                                        
+                                        <td class="px-4 py-4 text-sm break-words whitespace-normal">
+                                            asdasdasda sdas das das das da das d a da a sdas das das da sdas dikas idais jdia jisdjasi dja  asd asd asi dhiasj dija isdjiasjid jasi djai djia sjdijai sjdiasj id jais jdia sjid jai djai jdi ajsidjias jiasj dijasd 
+                                        </td>
+
+                                        <td class="px-4 py-4 text-sm whitespace-nowrap">
+                                            <a href="" class="px-1 py-1 text-gray-500 transition-colors duration-200 rounded-lg hover:bg-gray-100 hover:rounded-full">
+                                                <div class="inline px-3 py-1 text-sm font-normal text-gray-700 rounded-full gap-x-2 bg-gray-200/60">
+                                                    Edit
+                                                </div>
+                                            </a>
+
+                                            <a href="" class="px-1 py-1 text-gray-500 transition-colors duration-200 rounded-lg hover:bg-gray-100 hover:rounded-full">
+                                                <div class="inline px-3 py-1 text-sm font-normal text-gray-700 bg-red-400 rounded-full gap-x-2">
+                                                    Delete
+                                                </div>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </div>
